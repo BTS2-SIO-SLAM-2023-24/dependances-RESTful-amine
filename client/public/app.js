d4c8b798e48cdbd1f724303d3a1cb0ad5b0fb67f
amineb213 <bensadi.amine18@gmail.com>
2023-09-27 10:56:40 +0200

    update

--- client/public/app.js
+++
@@ -1,7 +0,0 @@
-const chalk = require('chalk');
-const dotenv = require('dotenv');
-const express = require('express');
-const Joi = require('joi');
-const mongoose = require('mongoose');
-
-document.getElementById("messageContainer").textContent = "Hello !";
